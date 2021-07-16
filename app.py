37ba778c0075e543512c526ec14d0ca2ed5d1d8e
palashmunshi <palashmunshi4@gmail.com>
2021-07-17 02:50:18 +0530

    importing the db database at the top of app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemsList
 from resources.store import Store, StoreList
-
+from db import db
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydata.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
@@ -26,7 +26,7 @@ api.add_resource(Item, '/item/<string:name>')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
-    from db import db
+    
     db.init_app(app)
     app.run(port=5000, debug=True)
 
