e89cd074448b401983b88b92d4aa0f992aa62918
palashmunshi <palashmunshi4@gmail.com>
2021-07-17 19:40:55 +0530

    adding the comment(line17) for using locally

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ app = Flask(__name__)
 db_url = os.environ.get('DATABASE_URL')
 
 app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("://", "ql://", 1)
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///mydata.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 app.secret_key = 'palash'   
 api= Api(app)
