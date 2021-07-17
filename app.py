32e1963d5c57f96b287e4175ef8011498397332c
palashmunshi <75259660+palashmunshi@users.noreply.github.com>
2021-07-17 18:08:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,9 +23,10 @@ api.add_resource(ItemsList, '/items' )
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(UserRegister, '/register')
 
+db.init_app(app)
 if __name__ == '__main__':
     
-    db.init_app(app)
+    
     app.run(port=5000, debug=True)
 
     
